e70f3c3e9c72fea7bc2571dd8b88080d604e9bec
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-07-24 00:51:34 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pt-admin-v7-8';
+const CACHE='pt-admin-v8-8';
 const ASSETS=['./','index.html','admin.css','admin.js','firebase-config.js','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
